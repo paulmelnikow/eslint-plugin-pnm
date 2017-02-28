10ef5f03fa0dffa59e148cf6ee8a21200fe7e2c0
Paul Melnikow <email@paulmelnikow.com>
2017-02-28 12:57:58 -0500

    Brave new world: no semicolons

--- test/test.js
+++ test/test.js
@@ -1,68 +1,70 @@
 /* eslint no-unused-vars: "off", no-undef: "off", no-console: "off" */
 /* eslint no-redeclare: "off" */
 
-'use strict';
+'use strict'
 
-var compositeThing = new CompositeThing();
-var params = { thisIsPreferred: true };
-var params = { this_is_okay: true };
+var compositeThing = new CompositeThing()
+var params = { thisIsPreferred: true }
+var params = { this_is_okay: true }
 
 if (cond) {
-  stuff();
+  stuff()
 } else {
-  otherStuff();
+  otherStuff()
 }
 for (var i = 0; i < target; ++i) {
-  stuff();
+  stuff()
 }
 var foo = function (arg1, arg2) {
-  stuff(arg1);
-};
+  stuff(arg1)
+}
 
 var factorial = function (value) {
-  return value === 0 ? 1 : value * factorial(value - 1);
-};
-console.log(factorial(5));
+  return value === 0 ? 1 : value * factorial(value - 1)
+}
+console.log(factorial(5))
 
 var foo = [
   'one',
   'two',
   'three',
-];
-var foo = ['one', 'two', 'three'];
+]
+var foo = ['one', 'two', 'three']
 var bar = {
   one: 1,
   two: 2,
   three: 3,
-};
-var bar = { one: 1, two: 2, three: 3 };
+}
+var bar = { one: 1, two: 2, three: 3 }
 var baz = { bazinga: {
   one: 1,
   two: 2,
-} };
-foo.bar = 123;
+} }
+foo.bar = 123
 
-var filter = function (item) { return item.count > 1; };
+var filter = function (item) { return item.count > 1 }
 if (foo) {
-  something();
+  something()
 } else {
-  somethingElse();
+  somethingElse()
 }
 
-var result;
-result = 5 * (1 + 2);
-result = ! obj.isValid();
-result = ++count;
-result = count++;
-result = offset(-width, -height);
-result = +bar;
-result = cond ? first : second;
+var result
+result = 5 * (1 + 2)
+result = ! obj.isValid()
+result = ++count
+result = count++
+result = offset(-width, -height)
+result = +bar
+result = cond ? first : second
 
 var x = 'this is a' +
-  'multi-line string';
+  'multi-line string'
 
 
-const o = a => a;
-const y = () => {};
-const z = (a, b, c) => a;
-const w = i => ({ foo: i });
+const o = a => a
+const y = () => {}
+const z = (a, b, c) => a
+const w = i => ({ foo: i })
+
+;[1, 2, 3].forEach(a => { console.log(a) })
